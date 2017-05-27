7ee3c539cece1d8b9a5a3041a0faaaa0556e2ad1
Yulia Yushkova <yuselia@MacBook-Air-Yushkova.local>
2017-05-28 01:34:30 +0500

    Задание 1. Первая попытка.

---
+++ TestProject1/TestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+
+namespace TestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://www.google.com";
+            driver.FindElement(By.Name("q")).SendKeys("Webdriver");
+            wait.Until(ExpectedConditions.TitleIs("Webdriver - Поиск в Google"));
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
