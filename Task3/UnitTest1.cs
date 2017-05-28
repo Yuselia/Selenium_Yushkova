9db4ecae63dc0d25627bd1111c2fd5d44c4bd8b0
Yulia Yushkova <yuselia@MacBook-Air-Yushkova.local>
2017-05-29 01:12:52 +0500

    Задание 3
    Сценарий логина

---
+++ Task3/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+
+namespace TestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://localhost/litecart/admin/";
+            wait.Until(ExpectedConditions.TitleIs("My Store"));
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+            wait.Until(ExpectedConditions.ElementExists(By.XPath("(//li[@id='app-']/a/span[2])[3]")));
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
